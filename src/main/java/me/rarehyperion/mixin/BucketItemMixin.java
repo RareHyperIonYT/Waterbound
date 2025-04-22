f02afe15fb84b666e438a67cc8a617d14d866e21
RareHyperIon <rarehyperionyt@gmail.com>
2025-04-22 12:26:59 +0100

    Allow water to be placed in the nether. I lied in the previous commit, I definitely overlooked this, lmao.

---
+++ src/main/java/me/rarehyperion/mixin/BucketItemMixin.java
@@ -0,0 +1,20 @@
+package me.rarehyperion.mixin;
+
+import net.minecraft.item.BucketItem;
+import net.minecraft.world.dimension.DimensionType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(BucketItem.class)
+public class BucketItemMixin {
+
+    @Redirect(method = "placeFluid", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/dimension/DimensionType;ultrawarm()Z"
+    ))
+    public boolean allowNetherPlacement(final DimensionType instance) {
+        return false;
+    }
+
+}
