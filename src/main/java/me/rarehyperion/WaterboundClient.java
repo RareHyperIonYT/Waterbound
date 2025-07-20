fed16aeb2ca73bcd43d450198146e82bce651a9c
RareHyperIon <rarehyperionyt@gmail.com>
2025-07-20 12:36:54 +0100

    Updated to 1.21.8

--- src/main/java/me/rarehyperion/WaterboundClient.java
+++ src/main/java/me/rarehyperion/WaterboundClient.java
@@ -2,22 +2,21 @@ package me.rarehyperion;
 
 import me.rarehyperion.core.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.minecraft.client.render.RenderLayer;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.BlockRenderLayer;
 
 public class WaterboundClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        final BlockRenderLayerMap layerMap = BlockRenderLayerMap.INSTANCE;
-        layerMap.putBlock(ModBlocks.GLOW_INK_OAK_SAPLING, RenderLayer.getCutout());
-        layerMap.putBlock(ModBlocks.GLOW_INK_SPRUCE_SAPLING, RenderLayer.getCutout());
-        layerMap.putBlock(ModBlocks.GLOW_INK_CHERRY_SAPLING, RenderLayer.getCutout());
-        layerMap.putBlock(ModBlocks.GLOW_INK_DARK_OAK_SAPLING, RenderLayer.getCutout());
-        layerMap.putBlock(ModBlocks.GLOW_INK_ACACIA_SAPLING, RenderLayer.getCutout());
-        layerMap.putBlock(ModBlocks.GLOW_INK_BIRCH_SAPLING, RenderLayer.getCutout());
-        layerMap.putBlock(ModBlocks.GLOW_INK_JUNGLE_SAPLING, RenderLayer.getCutout());
-        layerMap.putBlock(ModBlocks.GLOW_INK_PALE_OAK_SAPLING, RenderLayer.getCutout());
+        BlockRenderLayerMap.putBlock(ModBlocks.GLOW_INK_OAK_SAPLING, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(ModBlocks.GLOW_INK_SPRUCE_SAPLING, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(ModBlocks.GLOW_INK_CHERRY_SAPLING, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(ModBlocks.GLOW_INK_DARK_OAK_SAPLING, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(ModBlocks.GLOW_INK_ACACIA_SAPLING, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(ModBlocks.GLOW_INK_BIRCH_SAPLING, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(ModBlocks.GLOW_INK_JUNGLE_SAPLING, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(ModBlocks.GLOW_INK_PALE_OAK_SAPLING, BlockRenderLayer.CUTOUT);
     }
 
 }
